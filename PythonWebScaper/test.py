9ac30d961dbf3aa01249e327cd67bc1ce6b6acab
Dustin Bell <dustinb104@gmail.com>
2025-08-23 23:38:00 -0500

    Not yet fixed

--- PythonWebScaper/test.py
+++ PythonWebScaper/test.py
@@ -9,7 +9,7 @@ from selenium import webdriver
 
 # url = "https://www.nba.com/players"
 url = "https://www.nba.com/stats/players/traditional?PerMode=Totals&sort=PTS&dir=-1"
-file_path = r"C:\Users\dusti\Documents\Programming\Python\PythonWebScaper"
+file_path = r"C:\Users\dusti\Documents\Programming\Python\PythonWebScaper\Concussion_Injuries_2012_2014.xlsx"
 
 data = re.search(r'({"props":.*})', requests.get(url).text).group(0)
 data = json.loads(data)
@@ -20,5 +20,5 @@ data = json.loads(data)
 with open(file_path, 'w') as file:
     json.dump(data,file, indent=4)
 
-df = pd.DataFrame(data["props"]["pageProps"]["players"])
-# print(df.head().to_markdown())
+df = pd.DataFrame(data)
+print(df.head())
